8b343aa1f589313d448996108e2b4fe90fe60579
brunomedeiros.sbu@gmail.com <brunomedeiros.sbu@gmail.com@c7865e5a-d968-5c08-811e-3384da59fbae>
2011-10-15 12:37:26 +0000

    Adicionando app no setting.

--- trunk/apps/livro/views.py
+++ trunk/apps/livro/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+
+
+def home(request):
+  retorno = ['Primeiro', 'Segundo', 'Terceiro', 'Quarta']
+  return render_to_response("PageLivro.html", {"transacoes": retorno});
--- trunk/settings.py
+++ trunk/settings.py
@@ -97,6 +97,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.staticfiles',
     
     'apps.home',
     'apps.livro',
